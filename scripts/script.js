a3c0caf27b426649f16dadcd165cf628eb1e4dc8
fcissem <faballa.cisse@gmail.com>
2020-06-14 22:35:00 +0200

    desktop dev completed

---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+//Accessing the DOM elements we'll use for this project
+
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const errorMessage = document.querySelector('span');
+const errorIcon = document.querySelector('.main_text-section_form--img');
+
+//Validating the form 
+
+button.addEventListener('click', () => {
+    event.preventDefault();
+    let email = input.value;
+    let isValid = validateEmail(email);
+
+    if (!isValid) {
+        input.style.border = '2px solid #F96464';
+        errorMessage.style.display = 'block';
+        errorIcon.style.display = 'initial';
+
+    } else {
+        ''
+
+    }
+});
